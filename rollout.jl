bd01aa349dafd51d9770ce231b241f896ddfe5f6
Darian Nwankwo <nwankwodarian@gmail.com>
2023-02-06 14:50:29 -0500

    investigation longer horizon

--- rollout.jl
+++ rollout.jl
@@ -57,9 +57,13 @@ function rollout!(T::Trajectory, lbs::Vector{Float64}, ubs::Vector{Float64};
         
         # Compute variations in xnext and update surrogate, perturbed surrogate,
         # and multioutput surrogate
-        sxnext = T.s(xnext)
-        # δsxnext = -sxnext.HEI \ T.δs(sxnext, T.fantasy_ndx).∇EI
-        δsxnext = -sxnext.HEI \ T.δs(sxnext).∇EI
+        try
+            sxnext = T.s(xnext)
+            # δsxnext = -sxnext.HEI \ T.δs(sxnext, T.fantasy_ndx).∇EI
+            δsxnext = -sxnext.HEI \ T.δs(sxnext).∇EI
+        catch e 
+            println("Error: ", e, " at xnext = ", xnext)
+        end
 
         # Update hessian if a new best is found on trajectory
         if fi < fbest
