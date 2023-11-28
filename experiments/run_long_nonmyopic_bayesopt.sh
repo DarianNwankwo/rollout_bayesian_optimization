023ca3edaa0439b5c55d444196a79916c7f02ee9
DarianNwankwo <dnwankwo404@gmail.com>
2023-11-28 16:00:56 -0500

    experimental script

---
+++ experiments/run_long_nonmyopic_bayesopt.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Define an array of experiment configurations
+
+myopic_configurations=(
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name schwefel3d"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name braninhoo"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name styblinskitang2d"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name holdertable"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name eggholder"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name bukinn6"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name rosenbrock"
+)
+
+nonmyopic_configurations=(
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name schwefel3d --horizon 0 --batch-size 8"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name braninhoo --horizon 0 --batch-size 8"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name styblinskitang2d --horizon 0 --batch-size 8"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name holdertable --horizon 0 --batch-size 8"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name eggholder --horizon 0 --batch-size 8"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name bukinn6 --horizon 0 --batch-size 8"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name rosenbrock --horizon 0 --batch-size 8"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name schwefel3d --horizon 1 --batch-size 8"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name braninhoo --horizon 1 --batch-size 8"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name styblinskitang2d --horizon 1 --batch-size 8"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name holdertable --horizon 1 --batch-size 8"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name eggholder --horizon 1 --batch-size 8"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name bukinn6 --horizon 1 --batch-size 8"
+    "--output-dir longrun-bayesopt --optimize --starts 8 --budget 60 --trials 50 --function-name rosenbrock --horizon 1 --batch-size 8"
+)
+
+# Loop over the experiment configurations and run each one in the background
+for config in "${myopic_configurations[@]}"; do
+  julia myopic_bayesopt.jl $config
+done
+
+for config in "${nonmyopic_configurations[@]}"; do
+  julia nonmyopic_bayesopt.jl $config
+done
--- experiments/run_nonmyopic_bo_with_optimal_surrogates.sh
+++ experiments/run_nonmyopic_bo_with_optimal_surrogates.sh
@@ -3,31 +3,8 @@
 # Define an array of experiment configurations
 
 configurations=(
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 0 --function-name gramacylee"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 0 --function-name goldsteinprice"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 0 --function-name sixhump"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 0 --function-name braninhoo"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 0 --function-name rosenbrock"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name gramacylee"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name ackley4d"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name sixhump"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name braninhoo"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name rosenbrock"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name rastrigin1d"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name rastrigin4d"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name easom"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name styblinskitang2d"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name bukinn6"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name eggholder"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name holdertable"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name schwefel3d"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name schwefel4d"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name levyn13"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name trid1d"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name trid4d"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name mccormick"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name hartmann3d"
-    "--output-dir data-with-optimal-surrogate --optimize --budget 15 --trials 50 --horizon 1 --function-name goldsteinprice"
+    "--output-dir dwos-filler --optimize --budget 15 --trials 50 --horizon 1 --function-name levyn13 --seed 275"
+    "--output-dir dwos-filler --optimize --budget 15 --trials 50 --horizon 1 --function-name goldsteinprice --seed 10096"
 )
 
 # Loop over the experiment configurations and run each one in the background
